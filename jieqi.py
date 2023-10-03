dce172baa04dc849d4327fec48f10c9b57b965b8
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-10-03 20:35:18 +0800

    Update jieqi.py

--- jieqi.py
+++ jieqi.py
@@ -12,6 +12,11 @@ import eacal
 from sxtwl import fromSolar
 from datetime import datetime
 from itertools import cycle, repeat
+import re
+from lunar_python import Solar
+from datetime import datetime
+import opencc
+
 
 tiangan = list('甲乙丙丁戊己庚辛壬癸')
 dizhi = list('子丑寅卯辰巳午未申酉戌亥')
@@ -114,23 +119,42 @@ def find_jq(year, month, day):
             return jqmc[dd.getJieQi()]
             break
 
-def jq(year, month, day, hour, minute):
-    solar_termlist = dict(zip([i[0] for i in c_t.get_annual_solar_terms(year)], [i[2] for i in c_t.get_annual_solar_terms(year)]))
-    jq1 = find_jq(year, month, day)
-    jq1_datetime = solar_termlist.get(jq1)
-    jq2 = solarterm_iter(jq1)[1]
-    jq2_datetime = solar_termlist.get(jq2)
-    jq3 = solarterm_iter(jq1)[-1]
-    jq3_datetime = solar_termlist.get(jq3)
-    cdatetime = datetime(year, month, day, hour, minute, 0)
-    a = cdatetime > jq1_datetime.replace(tzinfo=None)
-    b = cdatetime > jq2_datetime.replace(tzinfo=None)
-    if a == True:
-        if b == False:
-            return jq1
-        if b == True:
-            return jq2
+def jq(year, month, day, hour, minute): #'{}-{}-{} {}:{}:00'
+    lunar = Solar.fromYmd(year, month, day).getLunar()
+    jieQi = lunar.getJieQiTable()
+    converter = opencc.OpenCC('s2t.json')
+    jq_list = [jieQi[k].toYmdHms() for k in lunar.getJieQiList()]
+    jq_dict = {jieQi[k].toYmdHms():k for k in lunar.getJieQiList()}
+    #'1895-12-22 09:38:44'
+    target_datetime_str = '{}-{}-{} {}:{}:00'.format(str(year).zfill(4), str(month).zfill(2), str(day).zfill(2), str(hour).zfill(2), str(minute).zfill(2))
+    target_datetime = datetime.strptime(target_datetime_str, '%Y-%m-%d %H:%M:%S')
+    datetime_list = [datetime.strptime(s, '%Y-%m-%d %H:%M:%S') for s in jq_list]
+    def closest(lst, K): 
+        return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))] 
+    closest_date = closest(datetime_list, target_datetime)
     
+    def convert_key_to_value(object):
+        jqeng = {"DONG_ZHI":"冬至", "XIAO_HAN":"小寒", "DA_HAN":"大寒", "LI_CHUN":"立春","YU_SHUI":"雨水","JING_ZHE":"驚蟄","DA_XUE":"大雪"}
+        if object in jqeng:
+            return jqeng[object]
+        else:
+            return object
+    jie_qi = convert_key_to_value(jq_dict.get(str(closest_date)))
+    t_o_f = target_datetime > closest_date
+    if  t_o_f == True:
+        return converter.convert(jie_qi) 
+    if t_o_f == False and hour==closest_date.hour and minute!=closest_date.minute:
+        return converter.convert(new_list(jieqi_name,jie_qi)[-1])
+    if t_o_f == False and hour==closest_date.hour and minute==closest_date.minute:
+        return converter.convert(jie_qi)
+    if t_o_f == False and hour>closest_date.hour:
+        return converter.convert(jie_qi)
+    if t_o_f == False and hour<closest_date.hour:
+        return converter.convert(new_list(jieqi_name,jie_qi)[-1])
+    #if target_datetime > closest_date == False:
+    #    return 
+    
+
 def jiazi():
     jiazi = [tiangan[x % len(tiangan)] + dizhi[x % len(dizhi)] for x in range(60)]
     return jiazi
