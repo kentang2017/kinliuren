b63330f7b245f6cb617fbba377ce6c5b594785bb
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2025-08-27 23:30:07 +0800

    Update jieqi.py

--- jieqi.py
+++ jieqi.py
@@ -10,6 +10,7 @@ from itertools import cycle, repeat
 import  sxtwl
 from sxtwl import fromSolar
 import ephem
+from ephem import Sun, Date, Ecliptic, Equatorial
 
 
 jqmc = ['小寒', '大寒', '立春', '雨水', '驚蟄', '春分', '清明', '穀雨', '立夏', '小滿', '芒種', '夏至', '小暑', '大暑', '立秋', '處暑', '白露', '秋分', '寒露', '霜降', '立冬', '小雪', '大雪', '冬至']
@@ -292,3 +293,4 @@ if __name__ == '__main__':
     print(jq(year, month, day, hour, minute))
 
         
+
